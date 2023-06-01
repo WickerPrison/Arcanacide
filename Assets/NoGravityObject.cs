5985f9533c2eb6093e2caeda656dac244fc4967b
WickerPrison <92000023+WickerPrison@users.noreply.github.com>
2023-06-01 08:27:53 -0600

    Created floating objects
    also started decorating the 5th floor

---
+++ Assets/NoGravityObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoGravityObject : MonoBehaviour
+{
+    float xRotation;
+    float yRotation;
+    float zRotation;
+    float rotateSpeed;
+    Vector3 rotateVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRotation = Random.Range(-1f, 1f);
+        yRotation = Random.Range(-1f, 1f);
+        zRotation = Random.Range(-1f, 1f);
+        rotateSpeed = Random.Range(10f, 20f);
+        rotateVector = new Vector3(xRotation, yRotation, zRotation).normalized * rotateSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateVector);
+    }
+}
