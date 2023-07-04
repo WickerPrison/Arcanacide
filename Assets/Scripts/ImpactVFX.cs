a83a912f8b4711ac031f60d4249b28de45186f6d
WickerPrison <92000023+WickerPrison@users.noreply.github.com>
2023-07-04 13:39:57 -0600

    Reworked enemy vfx to follow body during animations

--- Assets/Scripts/ImpactVFX.cs
+++ Assets/Scripts/ImpactVFX.cs
@@ -4,16 +4,56 @@ using UnityEngine;
 
 public class ImpactVFX : MonoBehaviour
 {
+    [SerializeField] Transform frontPoint;
+    [SerializeField] Transform backPoint;
+    [SerializeField] bool testingOn = false;
+    [SerializeField] bool testingFaceFront;
     [SerializeField] PlayerData playerData;
     [SerializeField] EmblemLibrary emblemLibrary;
     [SerializeField] ParticleSystem magicImpact;
     [SerializeField] ParticleSystem dot;
     [SerializeField] ParticleSystem hitVFX;
     EnemyEvents enemyEvents;
+    EnemyController enemyController;
 
     private void Awake()
     {
         enemyEvents = GetComponentInParent<EnemyEvents>();
+    }
+
+    private void Start()
+    {
+        enemyController = GetComponentInParent<EnemyController>();
+    }
+
+    private void Update()
+    {
+        if (enemyController.facingFront)
+        {
+            transform.position = frontPoint.position;
+            transform.rotation = frontPoint.rotation;
+        }
+        else
+        {
+            transform.position = backPoint.position;
+            transform.rotation = backPoint.rotation;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!testingOn) return;
+
+        if (testingFaceFront)
+        {
+            transform.position = frontPoint.position;
+            transform.rotation = frontPoint.rotation;
+        }
+        else
+        {
+            transform.position = backPoint.position;
+            transform.rotation = backPoint.rotation;
+        }
     }
 
     private void OnTakeDamage(object sender, System.EventArgs e)
