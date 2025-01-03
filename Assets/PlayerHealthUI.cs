c276dc9eea898cb446ae76bd23abec651e76baa4
WickerPrison <cobylax17@gmail.com>
2025-01-03 13:39:26 -0700

    added healthbar delay effect

--- Assets/PlayerHealthUI.cs
+++ Assets/PlayerHealthUI.cs
@@ -9,20 +9,26 @@ public class PlayerHealthUI : MonoBehaviour
     [SerializeField] RectTransform background;
     [SerializeField] RectTransform fill;
     [SerializeField] RectTransform fillMask;
-    [SerializeField] RectMask2D delayMask;
+    [SerializeField] RectTransform delayFill;
+    [SerializeField] RectTransform delayFillMask;
     [SerializeField] PlayerData playerData;
-    float maxWidth = 1354;
     float maskDiff = 4;
-    float maxFillWidth = 1350;
     float zeroFill = 9;
-    float healthRatio = 6f;
+    float oneHPWidth = 6f;
+    bool buffer = false;
+    WaitForEndOfFrame endOfFrame = new WaitForEndOfFrame();
+    float delay;
+    [SerializeField] float speed;
+
+
 
     private void Start()
     {
-        float borderScale = playerData.MaxHealth() * healthRatio - maskDiff;
+        float borderScale = playerData.MaxHealth() * oneHPWidth - maskDiff;
         border.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, borderScale);
         background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, borderScale);
         fillMask.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, borderScale - 2);
+        delayFillMask.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, borderScale - 2);
         GainHealth(playerData.health);
     }
 
@@ -33,7 +39,8 @@ public class PlayerHealthUI : MonoBehaviour
 
     void GainHealth(int health)
     {
-        fill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * healthRatio + zeroFill);
+        fill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * oneHPWidth + zeroFill);
+        delayFill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * oneHPWidth + zeroFill);
     }
 
     private void OnPlayerLoseHealth(GlobalEvents sender, int health)
@@ -43,7 +50,32 @@ public class PlayerHealthUI : MonoBehaviour
 
     void LoseHealth(int health)
     {
-        fill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * healthRatio + zeroFill);
+        if (!buffer)
+        {
+            StopAllCoroutines();
+            StartCoroutine(HealthbarDelay());
+        }
+        else delay = 0.5f;
+        fill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * oneHPWidth + zeroFill);
+    }
+
+    IEnumerator HealthbarDelay()
+    {
+        delayFill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, fill.rect.width);
+        buffer = true;
+        delay = 0.5f;
+        while(delay > 0)
+        {
+            delay -= Time.deltaTime;
+            yield return endOfFrame;
+        }
+
+        while(delayFill.rect.width > fill.rect.width)
+        {
+            delayFill.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, delayFill.rect.width - Time.deltaTime * speed);
+            yield return endOfFrame;
+        }
+        buffer = false;
     }
 
     private void OnEnable()
