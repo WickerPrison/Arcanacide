bdab25cae782af76dee26fd0cec2da5aeb36489a
WickerPrison <cobylax17@gmail.com>
2025-01-11 15:24:52 -0700

    decorated FirePath4.2

---
+++ Assets/Editor/BoxSetupEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BoxSetup))]
+public class BoxSetupEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BoxSetup boxSetup = target as BoxSetup;
+        if (GUILayout.Button("Randomize Rotation"))
+        {
+            Undo.RecordObject(boxSetup.transform, "Randomize Box Rotation");
+            boxSetup.transform.localRotation = Quaternion.Euler(RandomAngle(), RandomAngle(), RandomAngle());
+            PrefabUtility.RecordPrefabInstancePropertyModifications(boxSetup.transform);
+        }
+    }
+
+    float RandomAngle()
+    {
+        int randInt = Random.Range(0, 4);
+        return randInt * 90f;
+    }
+}
