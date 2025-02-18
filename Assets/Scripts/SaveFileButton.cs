ba292248b95a1f720f913c832802cc3d17e7584e
Coby Davis <cobylax17@gmail.com>
2025-02-17 18:25:16 -0700

    removed backwards compatibility for save files

--- Assets/Scripts/SaveFileButton.cs
+++ Assets/Scripts/SaveFileButton.cs
@@ -16,7 +16,6 @@ public class SaveFileButton : MonoBehaviour
     TextMeshProUGUI text;
     GameManager gm;
     string textString;
-    bool haveOldData = false;
     ButtonVibrate buttonVibrate;
 
     private void Start()
@@ -28,17 +27,6 @@ public class SaveFileButton : MonoBehaviour
 
         SaveData data = null;
 
-        if(fileID == 1)
-        {
-            data = SaveSystem.LoadGame("playerData");
-            if (data != null) haveOldData = true; 
-        }
-
-        if(!haveOldData)
-        {
-            data = SaveSystem.LoadGame("saveFile" + fileID.ToString());
-        }
-
         if(data == null)
         {
             textString += "Empty";
@@ -47,17 +35,8 @@ public class SaveFileButton : MonoBehaviour
         }
         else
         {
-            if (haveOldData)
-            {
-                textString += "Pre version 0.2.11 savefile";
-                dateText.text = "";
-                timeText.text = "";
-            }
-            else
-            {
-                dateText.text = data.date;
-                timeText.text = data.time;
-            }
+            dateText.text = data.date;
+            timeText.text = data.time;
         }
         text.text = textString;
     }
@@ -66,14 +45,6 @@ public class SaveFileButton : MonoBehaviour
     {
         if (menuData.loadGame)
         {
-            if (haveOldData)
-            {
-                gm.LoadGame("playerData");
-                playerData.saveFile = "saveFile1";
-                gm.SaveGame();
-                File.Delete(Application.persistentDataPath + "/playerData.sav");
-            }
-
             if(File.Exists(Application.persistentDataPath + "/" + "saveFile" + fileID.ToString() + ".sav"))
             {
                 gm.LoadGame("saveFile" +  fileID.ToString());
