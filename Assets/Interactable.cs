9ce74c29725fe3916126d8ae26f144a48f103054
Coby Davis <cobylax17@gmail.com>
2025-03-21 15:19:39 -0600

    placed refund shards

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -7,25 +7,31 @@ public class Interactable : MonoBehaviour
 {
     [SerializeField] GameObject message;
     [SerializeField] PlayerData playerData;
+    [SerializeField] bool canInteractWhileCombat = false;
     [System.NonSerialized] public bool active = true;
     Transform player;
     InputManager im;
     float playerDistance = 100;
     float interactDistance = 2;
     public event EventHandler onInteracted;
+    GameManager gm;
 
     void Start()
     {
         im = GameObject.FindGameObjectWithTag("GameManager").GetComponent<InputManager>();
         im.controls.Gameplay.Interact.performed += ctx => Interact();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if (!canInteractWhileCombat)
+        {
+            gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        }
     }
 
     private void Update()
     {
         playerDistance = Vector3.Distance(transform.position, player.position);
 
-        if (playerDistance <= interactDistance && active)
+        if (CanInteract())
         {
             message.SetActive(true);
         }
@@ -35,10 +41,17 @@ public class Interactable : MonoBehaviour
         }
     }
 
+    bool CanInteract()
+    {
+        if (!active) return false;
+        if (playerDistance > interactDistance) return false;
+        if (!canInteractWhileCombat && gm.awareEnemies > 0) return false;
+        return true;
+    }
+
     void Interact()
     {
-        if (!active) return;
-        if (playerDistance > interactDistance) return;
+        if (!CanInteract()) return;
         onInteracted?.Invoke(this, EventArgs.Empty);
     }
 }
