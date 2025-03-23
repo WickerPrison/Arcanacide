c115afa47d0465d28af19996b15913920dbaf7f3
Coby Davis <cobylax17@gmail.com>
2025-03-23 07:15:52 -0600

    setup basic assemblies

--- Assets/Scripts/HomingSoulmass.cs
+++ Assets/Scripts/HomingSoulmass.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using UnityEngine.Timeline;
 using FMODUnity;
 
 public class HomingSoulmass : MonoBehaviour
