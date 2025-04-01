65db07eab8374842605960961a60306a681e6b9f
Coby Davis <cobylax17@gmail.com>
2025-03-31 19:15:57 -0600

    fixed arcane step

--- Assets/Scripts/PathTrail.cs
+++ Assets/Scripts/PathTrail.cs
@@ -14,28 +14,54 @@ public class PathTrail : MonoBehaviour
     float damagePerSecond;
     float damage;
     bool dead = false;
+    float cd = 0;
+    float maxCd = 0.3f;
+    List<EnemyScript> touchingEnemies = new List<EnemyScript>();
+    [System.NonSerialized] public List<PathTrail> pathTrails;
 
     private void Start()
     {
+        if (!HasSpace())
+        {
+            Destroy(gameObject);
+            return;
+        }
+        if(pathTrails != null) pathTrails.Add(this);
         VFX = GetComponentInChildren<ParticleSystem>();
-        damagePerSecond = 3 + playerData.arcane * 0.6f;
+        damagePerSecond = 1 + playerData.arcane * 0.5f;
         fmodInstance = RuntimeManager.CreateInstance(fmodEvent);
         fmodInstance.set3DAttributes(RuntimeUtils.To3DAttributes(transform.position));
         fmodInstance.setTimelinePosition(Random.Range(0, 2000));
     }
 
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Enemy") && !dead)
+    //    {
+    //        EnemyScript enemyScript;
+    //        enemyScript = other.gameObject.GetComponent<EnemyScript>();
+    //        damage += damagePerSecond * Time.deltaTime;
+    //        if (damage > 1)
+    //        {
+    //            enemyScript.LoseHealth(Mathf.FloorToInt(damage), 0);
+    //            damage = 0;
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy") && !dead)
+        if (other.gameObject.CompareTag("Enemy"))
         {
-            EnemyScript enemyScript;
-            enemyScript = other.gameObject.GetComponent<EnemyScript>();
-            damage += damagePerSecond * Time.deltaTime;
-            if (damage > 1)
-            {
-                enemyScript.LoseHealth(Mathf.FloorToInt(damage), 0);
-                damage = 0;
-            }
+            touchingEnemies.Add(other.gameObject.GetComponent<EnemyScript>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            touchingEnemies.Remove(other.gameObject.GetComponent<EnemyScript>());
         }
     }
 
@@ -56,6 +82,34 @@ public class PathTrail : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+
+        damage += damagePerSecond * Time.deltaTime;
+
+        if(cd > 0 || damage < 1)
+        {
+            cd -= Time.deltaTime;
+            return;
+        }
+
+        cd = maxCd;
+        foreach(EnemyScript enemy in touchingEnemies)
+        {
+            enemy.LoseHealth(Mathf.FloorToInt(damage), 0);
+        }
+        damage = 0;
+    }
+
+    bool HasSpace()
+    {
+        if (pathTrails == null) return true;
+        foreach(PathTrail pathTrail in pathTrails)
+        {
+            if(Vector3.Distance(pathTrail.transform.position, transform.position) < 0.7)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     void Death()
@@ -64,5 +118,6 @@ public class PathTrail : MonoBehaviour
         VFX.Stop();
         fmodInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         fmodInstance.release();
+        if(pathTrails != null) pathTrails.Remove(this);
     }
 }
