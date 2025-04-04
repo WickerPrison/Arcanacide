16cfced871dc1cf1b66a44a86453096da12c8286
Coby Davis <cobylax17@gmail.com>
2025-04-04 16:28:32 -0600

    fixed ticket filer demo bug

--- Assets/Scripts/FileTicket.cs
+++ Assets/Scripts/FileTicket.cs
@@ -66,7 +66,7 @@ public class FileTicket : MonoBehaviour, IBlockDoors
         {
             ticketState = TicketState.FILED;
         }
-        else if(!mapData.miniboss1Killed)
+        else if(!mapData.miniboss1Killed && buildMode.buildMode != BuildModes.DEMO)
         {
             ticketState = TicketState.DISABLED;
         }
