c3a0038b692b2a31cf8d2d1140aa07bb579e869f
Coby Davis <cobylax17@gmail.com>
2025-04-09 07:09:01 -0600

    added slash projectile attack

---
+++ Assets/Scripts/ElectricSwordsmanController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricSwordsmanController : EnemyController
+{
+    StepWithAttack stepWithAttack;
+    FacePlayer facePlayer;
+
+    public override void Start()
+    {
+        base.Start();
+        stepWithAttack = GetComponent<StepWithAttack>();
+        facePlayer = GetComponent<FacePlayer>();
+    }
+
+    public override void EnemyAI()
+    {
+        base.EnemyAI();
+
+        if (state == EnemyState.IDLE)
+        {
+            //navAgent is the pathfinding component. It will be enabled whenever the enemy is allowed to walk
+            if (navAgent.enabled == true)
+            {
+                navAgent.SetDestination(playerScript.transform.position);
+            }
+
+            if (attackTime <= 0)
+            {
+                Attack();
+            }
+        }
+
+        if (attackTime > 0)
+        {
+            attackTime -= Time.deltaTime;
+        }
+    }
+
+    void Attack()
+    {
+        int num = Random.Range(0, 3);
+        if (num <= 1)
+        {
+            frontAnimator.SetBool("TripleAttack", false);
+            backAnimator.SetBool("TripleAttack", false);
+        }
+        else
+        {
+            frontAnimator.SetBool("TripleAttack", true);
+            backAnimator.SetBool("TripleAttack", true);
+        }
+        frontAnimator.Play("Attack");
+        backAnimator.Play("Attack");
+        state = EnemyState.ATTACKING;
+        attackTime = attackMaxTime;
+    }
+
+    public override void AttackHit(int smearSpeed)
+    {
+        smearScript.particleSmear(smearSpeed);
+        stepWithAttack.Step(0.15f);
+        base.AttackHit(smearSpeed);
+
+        EnemySlashProjectile projectile = Instantiate(projectilePrefab).GetComponent<EnemySlashProjectile>();
+        projectile.transform.position = facePlayer.attackPoint.position + Vector3.up * 1.3f;
+        projectile.direction = Vector3.Normalize(facePlayer.attackPoint.position - transform.position);
+        projectile.spellDamage = spellAttackDamage;
+        projectile.poiseDamage = spellAttackPoiseDamage;
+        projectile.enemyOfOrigin = enemyScript;
+    }
+}
---
+++ Assets/Scripts/EnemySlashProjectile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class EnemySlashProjectile : Projectile
+{
+    private void Start()
+    {
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    public override void HitObject(Collider collision)
+    {
+        
+    }
+
+    public override void HitPlayer(Collider collision)
+    {
+        PlayerScript playerScript;
+        playerScript = collision.gameObject.GetComponent<PlayerScript>();
+        playerScript.LoseHealth(spellDamage, EnemyAttackType.PROJECTILE, enemyOfOrigin);
+        playerScript.LosePoise(poiseDamage);
+        RuntimeManager.PlayOneShot(playerImpactSFX, impactSFXvolume, transform.position);
+    }
+}
