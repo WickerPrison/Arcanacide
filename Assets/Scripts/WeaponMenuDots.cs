7935ef1a44b01741989a7ec9971c1760979e385b
Coby Davis <cobylax17@gmail.com>
2026-01-03 06:35:04 -0700

    clean up weapon menu dot code

--- Assets/Scripts/WeaponMenuDots.cs
+++ Assets/Scripts/WeaponMenuDots.cs
@@ -7,7 +7,6 @@ public class WeaponMenuDots : MonoBehaviour
 {
     [SerializeField] List<Image> dots;
     WeaponScroll weaponScroll;
-    [SerializeField] float dotSpacing;
     [SerializeField] MapData mapData;
     Vector3 bigScale = new Vector3(0.14f, 0.14f, 0.14f);
     Vector3 smallScale = new Vector3(0.08f, 0.08f, 0.08f);
@@ -23,7 +22,6 @@ public class WeaponMenuDots : MonoBehaviour
         for (int i = 0; i < dots.Count; i++)
         {
             int diff = i - weaponScroll.position;
-            //dots[i].rectTransform.localPosition = SetXPos(dots[i].rectTransform.localPosition, diff * dotSpacing);
             if (diff == 0)
             {
                 dots[i].rectTransform.localScale = bigScale;
@@ -43,17 +41,14 @@ public class WeaponMenuDots : MonoBehaviour
         for (int i = 0; i < dots.Count; i++)
         {
             int diff = i - weaponScroll.position;
-            Vector3 dotPosition = SetXPos(dots[i].rectTransform.localPosition, diff * dotSpacing);
-
-            StartCoroutine(MoveDot(dots[i], dots[i].rectTransform.localPosition, diff == 0));
+            StartCoroutine(MoveDot(dots[i], diff == 0));
         }
     }
 
-    public IEnumerator MoveDot(Image dot, Vector3 destination, bool isSelected)
+    public IEnumerator MoveDot(Image dot, bool isSelected)
     {
         float timer = transitionTime;
         float ratio;
-        Vector3 startPos = dot.rectTransform.localPosition;
         Vector3 startScale = dot.rectTransform.localScale;
         Vector3 endScale = isSelected ? bigScale : smallScale;
         dot.color = Color.white;
@@ -65,16 +60,10 @@ public class WeaponMenuDots : MonoBehaviour
         {
             timer -= Time.unscaledDeltaTime;
             ratio = timer / transitionTime;
-            dot.rectTransform.localPosition = Vector3.Lerp(destination, startPos, ratio);
             dot.rectTransform.localScale = Vector3.Lerp(endScale, startScale, ratio);
             yield return new WaitForEndOfFrame();
         }
-        dot.rectTransform.localPosition = destination;
-    }
-
-    Vector3 SetXPos(Vector3 oldVector, float newX)
-    {
-        return new Vector3(newX, oldVector.y, oldVector.z);
+        dot.rectTransform.localScale = endScale;
     }
 
     private void OnEnable()
