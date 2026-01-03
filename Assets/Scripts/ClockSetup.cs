07275071814cf8f69a15e037d14d5c96252c21e1
Coby Davis <cobylax17@gmail.com>
2026-01-03 08:19:32 -0700

    chaos clocks

--- Assets/Scripts/ClockSetup.cs
+++ Assets/Scripts/ClockSetup.cs
@@ -6,14 +6,32 @@ public class ClockSetup : MonoBehaviour
 {
     [SerializeField] Transform minuteHand;
     [SerializeField] Transform hourHand;
+    [SerializeField] bool chaosClock = false;
     WaitForSeconds minute = new WaitForSeconds(60);
     float angle;
+    float hourSpeed;
+    float minuteSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         int randSec = Random.Range(0, 60);
-        StartCoroutine(TickTimer(new WaitForSeconds(randSec)));
+        if (!chaosClock)
+        {
+            StartCoroutine(TickTimer(new WaitForSeconds(randSec)));
+        }
+        else
+        {
+            hourSpeed = Random.Range(-5, 5);
+            minuteSpeed = Random.Range(-20, 20);
+        }
+    }
+
+    private void Update()
+    {
+        if (!chaosClock) return;
+        minuteHand.Rotate(new Vector3(0, 0, minuteSpeed * Time.deltaTime));
+        hourHand.Rotate(new Vector3(0, 0, hourSpeed * Time.deltaTime));
     }
 
     void TickHand()
