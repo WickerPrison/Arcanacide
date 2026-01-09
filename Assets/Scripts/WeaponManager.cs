17ca3384791a1e50321e955eebaaa43e7af4c9c6
Coby Davis <cobylax17@gmail.com>
2026-01-09 06:00:19 -0700

    fix lantern sprite bug

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -43,6 +43,12 @@ public class WeaponManager : MonoBehaviour
         SetWeapon(playerData.currentWeapon);
     }
 
+
+    private void Update()
+    {
+        Debug.Log(backWeaponSprites[1].activeSelf);
+    }
+
     public void AddWeaponMagicSource()
     {
         weaponMagicSources++;
