59743a0d4afa8ce5ede8c7c8af38a50f2e312002
Coby Davis <cobylax17@gmail.com>
2026-02-10 07:27:49 -0700

    miniboss 1 music updated

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -74,6 +74,7 @@ public class MusicManager : MonoBehaviour
 
     private void onPlayerDeath(object sender, System.EventArgs e)
     {
+        musicPlayer.ChangeOptBossState(OptBossState.DEATH);
         musicPlayer.ChangeState(MusicState.DEATH);
     }
 
@@ -82,9 +83,9 @@ public class MusicManager : MonoBehaviour
         musicPlayer.ChangeState(MusicState.BOSSVICTORY);
     }
 
-    private void Global_onWhistleblowerKilled(object sender, System.EventArgs e)
+    private void Global_onMinibossKilled(object sender, System.EventArgs e)
     {
-        musicPlayer.ChangeState(MusicState.BOSSVICTORY);
+        musicPlayer.ChangeOptBossState(OptBossState.VICTORY);
     }
 
     private void Global_onMinibossEndDialogue(object sender, System.EventArgs e)
@@ -96,15 +97,16 @@ public class MusicManager : MonoBehaviour
     {
         GlobalEvents.instance.onBossKilled += onBossKilled;
         GlobalEvents.instance.onPlayerDeath += onPlayerDeath;
-        GlobalEvents.instance.onWhistleblowerKilled += Global_onWhistleblowerKilled;
+        GlobalEvents.instance.onMinibossKilled += Global_onMinibossKilled;
         GlobalEvents.instance.onMinibossEndDialogue += Global_onMinibossEndDialogue;
     }
+
 
     private void OnDisable()
     {
         GlobalEvents.instance.onBossKilled -= onBossKilled;
         GlobalEvents.instance.onPlayerDeath -= onPlayerDeath;
-        GlobalEvents.instance.onWhistleblowerKilled -= Global_onWhistleblowerKilled;
+        GlobalEvents.instance.onMinibossKilled -= Global_onMinibossKilled;
         GlobalEvents.instance.onMinibossEndDialogue -= Global_onMinibossEndDialogue;
     }
 
